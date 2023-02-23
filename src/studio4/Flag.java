7ef2e612340bb8ae701222b4586c3e00fdbb5a4b
emmafeirstein <emmafeirstein@localhost>
2023-02-23 12:07:16 -0600

    Studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,14 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(Color.GREEN); 
+		StdDraw.filledRectangle(.5, .5, .3, .2);
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle(.5, .5, .3, .1);
+		StdDraw.setPenColor(Color.PINK); 
+		StdDraw.filledCircle(.5, .5, .1);
+		
+		
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,21 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String shapeType = in.next(); 
+		int R = in.nextInt();
+		int G = in.nextInt(); 
+		int B = in.nextInt(); 
+		
+		boolean idk = in.nextBoolean();
+		
+		double x = in.nextDouble(); 
+		double y = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeight = in.nextDouble();
+		
+		StdDraw.setPenColor(R,G,B);
+		StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
 		
 	}
 }
